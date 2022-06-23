186a767d40a253b6c81923b8573d37bfdb37c8af
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-23 12:33:38 +0200

    code for feature 2 "It must be able to show/hide event details"

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    collapsed: true
+  }
+
+  handleDetailsClick = () => {
+    this.state.collapsed
+      ? this.setState({ collapsed: false })
+      : this.setState({ collapsed: true });
+  };
+
+  dateNewFormat = (eventDate) => {
+    const newDate = `${new Date(eventDate)}`;
+    return newDate;
+  };
+
+  changeButtonText = () => {
+    if (this.state.collapsed === true) {
+      return "show details";
+    } else {
+      return "hide details"
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return (
+      <div className="event">
+        <h4 className="title">{event.summary}</h4>
+        <p className="start-time">{this.dateNewFormat(event.start.dateTime)}</p>
+        <p className="location">{event.location}</p>
+        {!this.state.collapsed && (
+          <p className="event-details">{event.description}</p>
+        )}
+        <button
+          className="btn-details"
+          onClick={this.handleDetailsClick}
+        >{this.changeButtonText()}
+        </button>
+      </div>
+    );
   }
 }
 
