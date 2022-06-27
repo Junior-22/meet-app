db75b1d0dee364ecc8e54972da5644fe9255239c
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-28 00:59:53 +0200

    Feature 1, end-to-end test

--- src/tests/EndToEnd.test.js
+++ src/tests/EndToEnd.test.js
@@ -36,3 +36,62 @@ describe("show and hide event details", () => {
 
 });
 
+describe("filter events by city", () => {
+
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    // await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  // test("when a user hasn't searched for a city, show upcoming events from all cities", async () => {
+  //   await page.waitForSelector(".EventList");
+  //   const eventCount = await page.$$(".event").length;
+  //   expect(eventCount).toBe(10);
+  // });
+
+  // test("a user should see a list of suggestions when they search for a city", async () => {
+  //   await page.waitForSelector(".CitySearch");
+  //   await page.type(".city", "Berlin");
+  //   const suggestionCount = await page.$$(".suggestions li").length;
+  //   expect(suggestionCount).toBe(2);
+  // });
+
+  // test("a user can select a city from the suggested list", async () => {
+  //   await page.reload();
+  //   await page.waitForSelector(".CitySearch");
+  //   await page.type(".city", "Berlin");
+  //   await page.click(".suggestions li");
+  //   const eventCount = await page.$(".event").length;
+  //   expect(eventCount).toBe(3);
+  // });
+
+  test("when a user hasn't searched for a city, show upcoming events from all cities", async () => {
+    const countEvents = await page.$$eval(".event", (element) => element.length);
+    expect(countEvents).toBe(10);
+  });
+
+  test("a user should see a list of suggestions when they search for a city", async () => {
+    await page.type(".city", "Berlin");
+    const countCities = await page.$$eval(".suggestions li", (element) => element.length);
+    expect(countCities).toBe(2);
+  });
+
+  test("a user can select a city from the suggested list", async () => {
+    await page.reload();
+    await page.type(".city", "Berlin");
+    await page.click(".suggestions li");
+    const countEvents = await page.$$eval(".event", (element) => element.length);
+    expect(countEvents).toBe(5);
+  });
+
+});
