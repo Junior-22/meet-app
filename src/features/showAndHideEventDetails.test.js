1683273bee2ff8b7ed17a9775cc5420d8cd1921c
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-27 23:02:07 +0200

    Feature 2, acceptance test
    update quotes

--- src/features/showAndHideEventDetails.test.js
+++ src/features/showAndHideEventDetails.test.js
@@ -11,52 +11,52 @@ defineFeature(feature, test => {
 
   let AppWrapper;
 
-  test('An event element is collapsed by default', ({ given, when, then }) => {
+  test("An event element is collapsed by default", ({ given, when, then }) => {
     given('the user is on the main page', () => {
       AppWrapper = mount(<App />);
     });
 
-    when('nothing is selected', () => {
+    when("nothing is selected", () => {
 
     });
 
-    then('the event details will be hidden', () => {
+    then("the event details will be hidden", () => {
       AppWrapper.update();
       expect(AppWrapper.find(".event-details")).toHaveLength(0);
     });
   });
 
-  test('The user can expand an event to see its details', ({ given, when, then }) => {
+  test("The user can expand an event to see its details", ({ given, when, then }) => {
     given('the user wants to see more info about an event', async () => {
       AppWrapper = await mount(<App />);
     });
 
-    when('the user clicks on that event', () => {
+    when("the user clicks on that event", () => {
       AppWrapper.update();
       AppWrapper.find(".btn-details").at(0).simulate("click");
     });
 
-    then('the hidden info for that event will be expanded', () => {
+    then("the hidden info for that event will be expanded", () => {
       AppWrapper.update();
       expect(AppWrapper.find(".event-details").text()).toEqual(mockData[0].description);
     });
   });
 
-  test('The user can collapse an event to hide its details', ({ given, when, then }) => {
+  test("The user can collapse an event to hide its details", ({ given, when, then }) => {
 
     let EventWrapper;
 
-    given('the user has seen the details and chooses to collapse the view', () => {
+    given("the user has seen the details and chooses to collapse the view", () => {
       EventWrapper = shallow(<Event event={mockData[0]} />);
       EventWrapper.setState({ collapsed: false });
     });
 
-    when('the user clicks on the expand view', () => {
+    when("the user clicks on the expand view", () => {
       EventWrapper.update();
       EventWrapper.find(".btn-details").simulate("click");
     });
 
-    then('the view should collapse', () => {
+    then("the view should collapse", () => {
       expect(EventWrapper.state("collapsed")).toBe(true);
       expect(EventWrapper.find(".event-details")).toHaveLength(0);
     });
