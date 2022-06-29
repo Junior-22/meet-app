4c7d41f33328f9f0de66efdd2f4b33d65cb853f9
Junior-22 <juniorfrancis2608@gmail.com>
2022-06-29 13:31:13 +0200

    Create Alert.js
    Add info and error alerts

---
+++ src/Alert.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontSize: this.fontSize
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "blue";
+    this.fontSize = "15px";
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "red";
+    this.fontSize = "15px";
+  }
+}
+
+export { InfoAlert, ErrorAlert };
