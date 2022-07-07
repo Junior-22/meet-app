330d96d234aae3b214f1e2d7b359e3ec61eb1eb1
Junior-22 <juniorfrancis2608@gmail.com>
2022-07-07 08:43:15 +0200

    update code

--- src/App.js
+++ src/App.js
@@ -83,7 +83,9 @@ class App extends Component {
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    // const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    let isTokenValid = await checkToken(accessToken)
+    isTokenValid = isTokenValid.error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
     this.setState({
