7b0a66aa94bd49c056a818e72c24f64aa13d3d43
Junior-22 <juniorfrancis2608@gmail.com>
2022-07-08 11:31:41 +0200

    add code for scatterChart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NumberOfEvents from "./NumberOfEvents";
 import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import { OfflineAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 class App extends Component {
   state = {
@@ -115,17 +116,20 @@ class App extends Component {
     this.mounted = false;
   }
 
-  // getData = () => {
-  //   const { locations, events } = this.state;
-  //   const data = locations.map((location) => {
-  //     const number = events.filter((event) => event.location === location).length;
-  //     const city = location.split(", ").shift()
-  //     return { city, number };
-  //   })
-  //   return data;
-  // }
+  // prepare data for chart
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(", ").shift()
+      return { city, number };
+    })
+    return data;
+  }
 
   render() {
+
+    // const { locations, NumberOfEvents, events } = this.state;
 
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
 
@@ -141,6 +145,20 @@ class App extends Component {
           NumberOfEvents={this.state.NumberOfEvents}
           updateEvents={this.updateEvents}
         />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+            <Scatter data={this.getData()} fill="#82ca9d" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
